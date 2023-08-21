3ce19bb466e9dbb8690303a75e3ccf1d94354fc0
Fabio Odaguiri <54047352+ODAGAMMXIX@users.noreply.github.com>
2023-08-21 11:54:10 -0300

    JavaStdInNStdOutI

---
+++ JavaStdInNStdOutI.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int Read01 = scan.nextInt();
+        int Read02 = scan.nextInt();
+        int Read03 = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(Read01);
+        System.out.println(Read02);
+        System.out.println(Read03);
+        // Complete this line
+        // Complete this line
+        scan.close();
+    }
+}
