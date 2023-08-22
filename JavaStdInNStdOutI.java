0abf3af0c4df09ff2c3cdcc29e6dd7560a4886f4
Fabio Odaguiri <54047352+ODAGAMMXIX@users.noreply.github.com>
2023-08-21 22:27:05 -0300

    Basics 3

---
+++ JavaStdInNStdOutI.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class JavaStdInNStdOutI {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int read01 = scan.nextInt();
+        int read02 = scan.nextInt();
+        int read03 = scan.nextInt();
+      
+
+        System.out.println(read01);
+        System.out.println(read02);
+        System.out.println(read03);
+        
+        scan.close();
+    }
+}
---
+++ ManipuladorString.java
@@ -0,0 +1,5 @@
+public class ManipuladorString {
+public static void main(String[] args) {
+    sout
+}    
+}
